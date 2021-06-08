70190b6ef8746cd6cc5a91986d09d790f916493f
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-06-07 22:40:45 -0400

    Fixing test_create_dashboard

--- tests/test_dashboards.py
+++ tests/test_dashboards.py
@@ -20,30 +20,19 @@ def test_get_dashboards():
     assert isinstance(json_response, dict)
     assert len(json_response.get('dashboards', [])) >= 1
 
-# def test_get_dashboard(jira_api_data):
-#     resp = Dashboards(jira_api_data).get()
-#     assert resp is not None
-#     assert resp.status_code == 200
+def test_create_dashboard():
+    payload = {
+        "name": f"Test dashboard {datetime.now()}",
+        "description": "Some test description",
+        "sharePermissions": []
+    }
+    resp = Dashboards().create(dashboard=payload)
+    assert resp is not None
+    assert resp.status_code == 200
 
-#     json_response = json.loads(resp.text)
-#     print(json_response, type(json_response))
-#     assert isinstance(json_response, dict)
-#     assert len(json_response.get('dashboards', [])) >= 1
-
-
-# def test_create_dashboard(jira_api_data):
-#     payload = json.dumps({
-#         "name": f"Test dashboard {datetime.now()}",
-#         "description": "Some test description",
-#         "sharePermissions": []
-#     })
-#     resp = Dashboards(jira_api_data).create(dashboard=payload)
-#     assert resp is not None
-#     assert resp.status_code == 200
-
-#     json_response = json.loads(resp.text)
-#     print(json_response, type(json_response))
-#     assert isinstance(json_response, dict)
+    json_response = json.loads(resp.text)
+    print(json_response, type(json_response))
+    assert isinstance(json_response, dict)
 
 
 # def test_reject_create_dashbaord(jira_api_data):
